f398306a44dc5e1f5b01f4e7baed74858416ac45
Methsara Premarathna <45135715+Metha99@users.noreply.github.com>
2025-05-12 13:59:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import openai
 
 # Hardcoded API Keys and Credentials (For testing purposes)
-openai.api_key = "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"
+openai.api_key = "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"
 
 # Azure credentials
 AZURE_ACCESS_TOKEN = "QxN8Q~y.PalckYVbS5evoch2u3HiPfmhT1LmfbqX"
